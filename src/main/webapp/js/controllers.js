20d460552369222def8499c98f8545ce6ceb4591
zdenekstrbik <zdenek.strbik@gmail.com>
2014-08-13 13:41:00 -0400

    Added menu

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -3,19 +3,22 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-        controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
-                $scope.loadId = function loadId() {
-                    $http({method: 'GET', url: 'http://192.168.1.106/rest/training/load'}).
-                            success(function(data, status, headers, config) {
-                                $scope.randomId = data;
-                            }).
-                            error(function(data, status, headers, config) {
-                                $scope.randomId = 'ERROR!';
-                            });
-                }
+      controller('MyCtrl1', ['$scope', '$http', function ($scope, $http) {
 
-                $scope.loadId();
-            }])
-        .controller('MyCtrl2', [function() {
+         $scope.url = 'http://localhost:8070/rest/training/load';
 
-            }]);
+         $scope.loadId = function loadId() {
+            $http({method: 'GET', url: $scope.url}).
+                  success(function (data, status, headers, config) {
+                     $scope.randomId = data;
+                  }).
+                  error(function (data, status, headers, config) {
+                     $scope.randomId = 'ERROR! ' + status;
+                  });
+         }
+
+         $scope.loadId();
+      }])
+      .controller('MyCtrl2', [function () {
+
+      }]);
